3aee627779b94a497e2531d1bed6e0ee33913451
Sourabh Majoka <majokasourabh30@gmail.com>
2024-12-07 14:08:58 +0530

    fix deployment issue

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
-port = int(os.environ.get("PORT", 8080))
+port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
